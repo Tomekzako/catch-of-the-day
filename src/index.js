b3f7332c2baa27a385b6a07b88fc61b5ac7a43a9
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-08-27 13:21:57 +0200

    first component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import StorePicker from './components/StorePicker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render( <StorePicker />, document.querySelector('#main'));
