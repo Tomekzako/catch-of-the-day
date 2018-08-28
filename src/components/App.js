55e931c8bcf0698cd5932a85f6356a1e1c43ebd3
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-08-28 14:28:45 +0200

    Order, Inventory Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Inventory from './Inventory';
+import Order from './Order';
 
 
 class App extends Component {
@@ -9,6 +11,8 @@ class App extends Component {
                 <div className="menu">
                     <Header />
                 </div>
+                <Order />
+                <Inventory />
             </div>
         )
     }
