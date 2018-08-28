17dbc67804be3fd8817a119adecc6373a67d62fd
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-08-28 11:06:35 +0200

    Header Component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import StorePicker from './components/StorePicker';
+import App from './components/App';
 import './css/style.css';
 
 
-ReactDOM.render( <StorePicker />, document.querySelector('#main'));
+ReactDOM.render( <App />, document.querySelector('#main'));
