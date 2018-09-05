a17b4ab5573b299cce7a4f96a25b331ee23083e6
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-09-05 13:13:07 +0200

    Order Component

--- src/components/Order.js
+++ src/components/Order.js
@@ -3,7 +3,26 @@ import React, { Component } from 'react';
 
 class Order extends Component {
     render() {
-        return <div className="order">Order!!!</div>
+        const orderIds = Object.keys(this.props.order);
+        const total = orderIds.reduce((prev, cur) => {
+            const fish = this.props.fishes[cur];
+            const count = this.props.order[cur];
+            const isAvailable = fish && fish.status === 'available';
+            if(isAvailable) {
+                return prev + (count * fish.price);
+            }
+            return prev;
+            
+        }, 0);
+        return (
+            <div className="order-wrap">
+                <h2>Order</h2>
+                {orderIds}
+                <div className="total">
+                    <strong>{total}</strong>
+                </div>
+            </div>
+        );
     }
 }
 
