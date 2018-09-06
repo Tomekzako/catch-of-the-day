a8d4495ea1e773caa7dd60c9a95f3abf244f473a
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-09-06 10:14:30 +0200

    base.js

--- src/base.js
+++ src/base.js
@@ -1,2 +1,14 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyBxfypW_4BMkMlkaGEw1xZrXlgd3vxSQJ4",
+    authDomain: "catch-of-the-day-tomek-chyc.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-tomek-chyc.firebaseio.com"
+});
+
+const base = Rebase.createClass(firebaseApp.database());
+
+export { firebaseApp };
+
+export default base;
